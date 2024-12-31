bbd8eca1ab44c450df06fdbafa06fd8e4c92da34
VaishnaviLakade <lakadevaishu26@gmail.com>
2024-12-31 08:37:23 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("menu-icon").addEventListener("click", function () {
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("show");
+});
